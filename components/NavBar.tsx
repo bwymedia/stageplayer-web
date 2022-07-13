333d81b75435358d7d11e474952d610d29ce67b5
Robert Hogan <rhogan7@gmail.com>
2022-07-13 12:38:10 -0700

    Cleaned up hamburger menu code.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -45,25 +45,6 @@ const Navbar = ({ children }: Props): React.ReactNode => {
           <Menu>
             <MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon />} variant="transparent" />
             <MenuList py={0} zIndex={3} onMouseEnter={onOpen} onMouseLeave={onClose}>
-              {/* <MenuItem onMouseEnter={onOpen} onMouseLeave={onClose} p={0}>
-              <Select placeholder="StagePlayer Web" borderTop="0" borderLeft="0" borderRight="0" roundedBottom="0">
-                <option value="option1">About SP Web</option>
-                <option value="option2">Contact Us</option>
-              </Select>
-            </MenuItem>
-            <MenuItem p={0}>
-              <Select onMouseEnter={onOpen} onMouseLeave={onClose} placeholder="My Collections" borderTop="0" borderLeft="0" borderRight="0" roundedBottom="0">
-                <option value="option1">New Collection</option>
-                <option value="option2">View/Edit</option>
-              </Select>
-            </MenuItem>
-            <MenuItem onMouseEnter={onOpen} onMouseLeave={onClose} p={0}>
-              <Select placeholder="My Productions" borderTop="0" borderLeft="0" borderRight="0" roundedBottom="0">
-                <option value="option1">New Production</option>
-                <option value="option2">Active Productions</option>
-                <option value="option2">Retired Productions</option>
-              </Select>
-            </MenuItem> */}
               <MenuGroup title="StagePlayer Web">
                 <MenuItem>
                   <NextLink href="/">
